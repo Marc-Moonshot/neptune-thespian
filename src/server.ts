9f221626d06d8641462e3facee81484171e5c176
Marc-4 <blaze.verdugo.s@gmail.com>
2025-12-03 16:34:04 +0800

    init

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+// firestore init
+// connect to firestore
+// read & cache schedules collection
+// set control_data document based on schedule.value and schedule.time. do this check every n minutes
+import cors from "cors"
+// import express from "express"
+
+const port = 3000
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(port, () => {
+  console.log(`Thespian running on port ${port}`)
+})
---
+++ src/types/express.d.ts
@@ -0,0 +1,4 @@
+export type Schedule = {
+  time: number
+  value: number
+}
