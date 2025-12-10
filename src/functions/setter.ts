c677c55dfb43368215a8b69d0fd1eff384715c3c
Marc-4 <blaze.verdugo.s@gmail.com>
2025-12-10 11:33:44 +0800

    initialized setter function

--- src/functions/setter.ts
+++ src/functions/setter.ts
@@ -1,1 +1,11 @@
+import type { DocumentReference, Firestore } from "firebase-admin/firestore"
+import logger from "logger.ts"
+
 // sets the DeviceControlData document's control_value field to the parameter passed.
+export default async function setter(
+  db: Firestore,
+  updates: { value: number; docRef: DocumentReference }[]
+) {
+  for (const update of updates) {
+  }
+}
